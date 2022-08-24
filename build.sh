4f2244c2215263631f9f89b2827db037052c1695
lilrazlil <nikmoshkin.2002@gmail.com>
2022-08-24 10:24:20 +0700

    docker

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build  --build-arg STAMP="$(cat .git/refs/heads/master)+$(date +%c)" --pull --rm -f "golang-api/Dockerfile" -t go-api:latest "golang-api"
-docker build  --build-arg STAMP="$(cat .git/refs/heads/master)+$(date +%c)" --pull --rm -f "golang-db/Dockerfile" -t go-db:latest "golang-db"
